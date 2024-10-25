ae045ec6699407048c28567fe4bd7a73ca270d33
Dhruvin Lad <laddhruvin84@gmail.com>
2024-10-25 12:31:39 -0400

    Add project files.

---
+++ Gym/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WhyUs()
+        {
+            return View();
+        }
+        public IActionResult Trainer()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
